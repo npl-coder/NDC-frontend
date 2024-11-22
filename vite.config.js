1e3db8ffe66feaf19975d3caed03246575e41c5d
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2024-11-22 15:01:45 +0545

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Define __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +19,11 @@ export default defineConfig({
     include: /.*\.[tj]sx?$/
   },
   build: {
-    outDir: 'dist'
-  }
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notfound: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 })
